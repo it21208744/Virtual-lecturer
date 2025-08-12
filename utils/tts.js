8f0b347a94fa12a0c36f315261092adf53168595
Jayawickrama Y.R.C.S <chamodansajeewana@gmail.com>
2025-08-12 16:36:36 +0530

    Update tts.js

--- utils/tts.js
+++ utils/tts.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const util = require('util')
 const textToSpeech = require('@google-cloud/text-to-speech')
 
-// Creates a client
+
 const client = new textToSpeech.TextToSpeechClient()
 
 async function textToSpeechConvert(
@@ -12,17 +12,16 @@ async function textToSpeechConvert(
 ) {
   const request = {
     input: { text },
-    // Select the language and voice
+
     voice: { languageCode: 'en-US', name: voice },
-    // Select the type of audio encoding
+
     audioConfig: { audioEncoding: 'MP3', speakingRate: speed },
   }
 
-  // Performs the text-to-speech request
+
   const [response] = await client.synthesizeSpeech(request)
 
-  // Save the audio to a file or return the buffer
-  // For now, return audio content buffer
+
   return response.audioContent
 }
 
